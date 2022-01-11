d2926b3507b68962cbf02a37b06358507ad43bd3
Daniel Kwon <danielkwon89@gmail.com>
2022-01-10 19:32:40 -0500

    Created CardList component and CSS styling for CardList component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
 
-class App extends React.Component{
+class App extends React.Component {
   constructor() {
-    super()
-    
+    super();
+
     this.state = {
-      monsters: [
-        {name: 'Frankenstein'},
-        {name: 'Dracula'},
-        {name: 'Zombie'}
-      ]
-    }
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map((monster, i) => <h1 key={monster.name + "_" + i}>{monster.name}</h1>)
-        }
+        <CardList>
+          {this.state.monsters.map((monster, i) => (
+            <h1 key={monster.name + "_" + i}>{monster.name}</h1>
+          ))}
+        </CardList>
       </div>
     );
   }
